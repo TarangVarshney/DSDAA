546a61473e914d2cb5601974996afb8b175eb347
Tarang Varshney <tarang@192.168.1.12>
2021-04-24 16:01:47 +0530

    Insertion sort : Time complexity O(n2)

---
+++ zDSDAA/src/com/tarang/dsdaa/sorting/InsertionSort.java
@@ -0,0 +1,47 @@
+package com.tarang.dsdaa.sorting;
+
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+/*
+ * Time Complexity:O(n²) 
+ * Insertion sort is used when number of elements is small.
+ */
+public class InsertionSort {
+	
+	public static void main(String[] args) {
+		System.out.println("Enter numbers :");
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		
+		System.out.println("Unsorted array "+line);
+		
+		int[] arr = Stream.of(line.split("\\s+")).mapToInt(Integer::parseInt).toArray();
+		
+		int[] sorted = insertionSort(arr, arr.length);
+		
+		for (int i : sorted) {
+			System.out.print(i+" ");
+		}
+	}
+
+	private static int[] insertionSort(int[] arr, int n) {
+		
+		int i,j, key;
+		
+		for(i = 1;i<n;i++) {
+			
+			// SELECTS the first unsorted element
+			key = arr[i];
+			
+			j = i-1;
+			while(j>=0 && key < arr[j]) {
+				arr[j+1] =  arr[j];
+				j = j-1;
+			}
+			arr[j+1] = key;
+		}
+		
+		return arr;
+	}
+}
