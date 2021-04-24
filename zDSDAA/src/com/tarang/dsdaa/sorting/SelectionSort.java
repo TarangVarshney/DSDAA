bc31c78311abb7b0194081777fa866a64bec27c8
Tarang Varshney <tarang@192.168.1.12>
2021-04-24 11:26:29 +0530

    Selection Sort -- time complexity O(n2)

---
+++ zDSDAA/src/com/tarang/dsdaa/sorting/SelectionSort.java
@@ -0,0 +1,47 @@
+package com.tarang.dsdaa.sorting;
+
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class SelectionSort {
+	
+	public static void main(String[] args) {
+		System.out.println("Enter numbers :");
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		
+		System.out.println("Unsorted array "+line);
+		
+		int[] arr = Stream.of(line.split("\\s+")).mapToInt(Integer::parseInt).toArray();
+		
+		int[] sorted = selectionSort(arr);
+		
+		for (int i : sorted) {
+			System.out.print(i+" ");
+		}
+	}
+
+	private static int[] selectionSort(int[] arr) {
+		
+		int n = arr.length;
+		
+		for(int i =0; i<n-1;i++) {
+			int min_index = i;
+			
+			// SELECTION
+			for(int j = i+1; j<n-1;j++) {
+				if(arr[min_index] > arr[j]) 
+					min_index = j;
+			}
+			
+			// SWAP the found minimum element with the first element
+			int tmp = arr[min_index];
+			arr[min_index] = arr[i];
+			arr[i] = tmp;
+			
+			// SHIFT COUNTER i++
+		}
+		
+		return arr;
+	}
+}
